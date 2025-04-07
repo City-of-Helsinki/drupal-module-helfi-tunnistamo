544f00c4da5281b59d8b0c05f51af05acf3e8e15
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-04-07 09:12:37 +0300

    UHF-11337: Use DebugDataItem attribute

--- src/Plugin/DebugDataItem/Tunnistamo.php
+++ src/Plugin/DebugDataItem/Tunnistamo.php
@@ -6,52 +6,33 @@ namespace Drupal\helfi_tunnistamo\Plugin\DebugDataItem;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\helfi_api_base\Attribute\DebugDataItem;
 use Drupal\helfi_api_base\DebugDataItemPluginBase;
 use Drupal\helfi_tunnistamo\Plugin\OpenIDConnectClient\Tunnistamo as TunnistamoClient;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the debug_data_item.
- *
- * @DebugDataItem(
- *   id = "tunnistamo",
- *   label = @Translation("Tunnistamo"),
- *   description = @Translation("Tunnistamo")
- * )
  */
+#[DebugDataItem(
+  id: 'tunnistamo',
+  title: new TranslatableMarkup('Tunnistamo'),
+)]
 final class Tunnistamo extends DebugDataItemPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Constructs a new instance.
-   *
-   * @param array $configuration
-   *   The plugin configuration.
-   * @param string $plugin_id
-   *   The plugin id.
-   * @param array $plugin_definition
-   *   The plugin definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
-   *   The entity type manager service.
+   * The entity type manager.
    */
-  public function __construct(
-    array $configuration,
-    string $plugin_id,
-    array $plugin_definition,
-    private EntityTypeManagerInterface $entityTypeManager,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
+  private EntityTypeManagerInterface $entityTypeManager;
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) : self {
-    return new self(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager')
-    );
+    $instance = new self($configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get(EntityTypeManagerInterface::class);
+    return $instance;
   }
 
   /**
